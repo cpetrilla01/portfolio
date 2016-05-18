86fc5709f94b3b0437eb6aa67fdd9fbab7dd89c4
Christopher Petrilla <cmpetrilla@gmail.com>
2016-05-18 10:17:43 -0400

    Add CSS concatenation and minification. Rewrite resulting CSS source URL in index file.

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,26 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var htmlReplace = require('gulp-html-replace');
+
+var now = Date.now();
 
 gulp.task('copyTemplates', function() {
-	return gulp.src('./resources/templates/index.html').pipe(gulp.dest('dist/templates'));
+	return gulp.src('./resources/templates/index.html')
+		.pipe(htmlReplace({
+			'css': '/css/main' + now + '.min.css'
+		}))
+		.pipe(gulp.dest('dist/templates'));
 });
 
 gulp.task('compileLess', function () {
 	return gulp.src('./resources/less/**/*.less')
 		.pipe(sourcemaps.init())
 		.pipe(less())
+		.pipe(concat('main' + now + '.min.css'))
+		.pipe(cssmin())
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('./dist/css'));
 });
