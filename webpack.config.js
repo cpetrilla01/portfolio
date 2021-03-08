be6bebef785e08e4acfc6351575dca98567a34e9
Christopher Petrilla <cmpetrilla@gmail.com>
2021-03-08 00:22:08 -0500

    Change directory of built files

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   ],
   output: {
     filename: 'index.bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   module: {
     rules: [
