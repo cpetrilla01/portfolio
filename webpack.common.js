0888dae095a2ddc1cc502b35de75ed72e39e4760
Christopher Petrilla <cmpetrilla@gmail.com>
2021-03-18 00:11:34 -0400

    Add prod and dev Webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -6,10 +6,6 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
