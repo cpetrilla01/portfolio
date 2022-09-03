65677431afba7282c6e08fd8e823f73292e2cca9
Christopher Petrilla <cmpetrilla@gmail.com>
2022-09-03 16:45:57 -0400

    feat: use MUI for header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,35 +1,110 @@
-import React from 'react';
-import Wordmark from './Wordmark';
-import Navigation from './Navigation';
-import {createUseStyles} from 'react-jss';
-import {sectionStyles} from '../../styles/shared-styles';
+import React, {useState} from 'react';
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Link,
+  SwipeableDrawer,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import {Menu as MenuIcon} from '@mui/icons-material';
 
-const useStyles = createUseStyles({
-  container: {
-    ...sectionStyles,
-    minHeight: 0,
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    position: 'absolute',
-    color: 'white',
-  },
-});
+const navigationItemsData = [
+  {
+    id: '#skills',
+    label: 'Skills',
+ },
+  {
+    id: '#about',
+    label: 'About',
+ },
+];
 
-const Header = () => {
-  const styles = useStyles();
+export default function ButtonAppBar() {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
   return (
-    <header className={styles.container}>
-      <div>
-        <Wordmark />
-      </div>
-      <div>
-        <Navigation />
-      </div>
-    </header>
+    <Box
+      sx={{display: 'flex'}}
+    >
+      <AppBar
+        component="nav"
+      >
+        <Toolbar>
+          <Box
+            sx={{display: {sm: 'none'}}}>
+            <IconButton
+              aria-label='Open drawer'
+              color='inherit'
+              edge='start'
+              onClick={handleDrawerToggle}
+              size='large'
+              sx={{mr: 2}}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
+          <Typography
+            component='h1'
+            sx={{flexGrow: 1, textTransform: 'uppercase'}}
+            variant='h6'
+          >
+            <Link
+              color='inherit'
+              href='/'
+              underline='none'
+            >
+              Christopher Petrilla
+            </Link>
+          </Typography>
+          <Box
+            sx={{display: {xs: 'none', sm: 'block'}}}
+          >
+            {navigationItemsData.map((navigationItem) => (
+              <Button
+                color='inherit'
+                href={navigationItem.id}
+                key={navigationItem.id}
+                size='large'
+              >
+                {navigationItem.label}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+      >
+        <SwipeableDrawer
+          ModalProps={{keepMounted: true}}
+          open={mobileOpen}
+          onClose={() => setMobileOpen(false)}
+          onOpen={() => setMobileOpen(true)}
+          PaperProps={{sx: {py: 4, width: "80%"}}}
+          sx={{display: {xs: 'block', sm: 'none'}}}
+          variant="temporary"
+        >
+          {navigationItemsData.map((navigationItem) => (
+            <Link
+              href={navigationItem.id}
+              key={navigationItem.label}
+              onClick={() => setMobileOpen(false)}
+              sx={{p: 2, textAlign: 'center'}}
+              underline='none'
+              variant='h3'
+            >
+              {navigationItem.label}
+            </Link>
+          ))}
+        </SwipeableDrawer>
+      </Box>
+    </Box>
   );
-};
-
-export default Header;
+}
--- src/index.js
+++ src/index.js
@@ -2,21 +2,38 @@ import React from 'react';
 import {render} from 'react-dom';
 import 'normalize.css';
 import './styles/global.css';
+import {ThemeProvider, createTheme} from '@mui/material';
 import Header from './components/header/Header';
 import HeroSection from './components/hero-section/HeroSection';
 import AboutSection from './components/about-section/AboutSection';
 import SkillsSection from './components/skills-section/SkillsSection';
 import Footer from './components/footer/Footer';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0A72BA',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#033558',
+      contrastText: '#000',
+    },
+  },
+  typography: {
+    fontFamily: '"Source Sans 3", arial, sans-serif'
+  }
+});
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <HeroSection />
       <SkillsSection />
       <AboutSection />
       <Footer />
-    </>
+    </ThemeProvider>
   );
 }
 
