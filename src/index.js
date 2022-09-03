d837966f3a925dfe442b053c7a4aa65b6509dc58
Christopher Petrilla <cmpetrilla@gmail.com>
2022-09-03 18:00:16 -0400

    refactor: create view directory for home page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'normalize.css';
 import './styles/global.css';
 import {ThemeProvider, createTheme} from '@mui/material';
 import {Header} from './components/Header';
-import {HeroSection} from './components/sections';
+import {HeroSection} from './views/home';
 import AboutSection from './components/about-section/AboutSection';
 import SkillsSection from './components/skills-section/SkillsSection';
 import Footer from './components/footer/Footer';
