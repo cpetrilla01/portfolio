fb883c3a5d0cb3aeea79567466a058350197d336
Christopher Petrilla <cmpetrilla@gmail.com>
2022-09-05 13:48:04 -0400

    refactor: move theme to separate file

--- src/index.js
+++ src/index.js
@@ -1,52 +1,16 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import './styles/global.css';
+import {theme} from './styles/theme';
 import {ThemeProvider, createTheme, CssBaseline} from '@mui/material';
 import {Header, Footer} from './components';
 import {Home} from './views/home';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#0A72BA',
-      contrastText: '#fff',
-    },
-    secondary: {
-      main: '#033558',
-      contrastText: '#000',
-    },
-  },
-  typography: {
-    fontFamily: '"Source Sans 3", arial, sans-serif',
-    allVariants: {
-      color: '#444444',
-    },
-    body1: {
-      fontSize: '1.25rem',
-      marginBottom: '1rem',
-    },
-    body2: {
-      fontSize: '1rem',
-    },
-    h1: {
-      fontSize: '1.25rem',
-      textTransform: 'uppercase',
-    },
-    h2: {
-      paddingBottom: '2rem',
-      textAlign: 'center',
-    },
-    subtitle1: {
-      fontSize: '3rem',
-    },
-  }
-});
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={createTheme(theme)}>
         <Header />
         <Home />
         <Footer />
---
+++ src/styles/theme.js
@@ -0,0 +1,38 @@
+const theme = {
+	palette: {
+		primary: {
+			main: '#0A72BA',
+			contrastText: '#fff',
+		},
+		secondary: {
+			main: '#033558',
+			contrastText: '#000',
+		},
+	},
+	typography: {
+		fontFamily: '"Source Sans 3", arial, sans-serif',
+		allVariants: {
+			color: '#444444',
+		},
+		body1: {
+			fontSize: '1.25rem',
+			marginBottom: '1rem',
+		},
+		body2: {
+			fontSize: '1rem',
+		},
+		h1: {
+			fontSize: '1.25rem',
+			textTransform: 'uppercase',
+		},
+		h2: {
+			paddingBottom: '2rem',
+			textAlign: 'center',
+		},
+		subtitle1: {
+			fontSize: '3rem',
+		},
+	}
+};
+
+export {theme};
