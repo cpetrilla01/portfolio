def2282c3c1dc77d3aeb6e9c8805ca67e7c9f9a3
Christopher Petrilla <cmpetrilla@gmail.com>
2022-09-20 00:19:53 -0400

    feat: add content to Skills section

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import {SectionContainer} from '../common';
-import skillsListData from './skills-list-data';
+import {core} from './core';
+import {tools} from './tools';
+import {architecture} from './architecture';
 import {
+  Box,
   Card,
   Container,
   Grid,
   Typography
 } from '@mui/material';
+
+const skillsSections = [
+  {
+    label: 'Core technologies',
+    contents: [...core],
+  },
+  {
+    label: 'Tools',
+    contents: [...tools],
+  },
+  {
+    label: 'Architecture',
+    contents: [...architecture],
+  },
+];
 
 const Skills = () => (
   <SectionContainer id='skills'>
@@ -17,43 +35,55 @@ const Skills = () => (
       >
         Skills
       </Typography>
-      <Grid
-        container
-        spacing={4}
-      >
-        {skillsListData.map(({id, title, description}, index) =>
-          <Grid
-            item
-            key={index}
-            lg={3}
-            md={4}
-            sm={6}
-            xs={12}
+      {skillsSections.map((section, index) =>
+        <div key={index}>
+          <Typography
+            variant='h3'
+            my={4}
           >
-            <Card
-              elevation={3}
-              sx={{
-                height: '100%',
-                p: 2,
-              }}
+            {section.label}
+          </Typography>
+          <Box>
+            <Grid
+              container
+              spacing={4}
             >
-              {id}
-              <Typography
-                component='h3'
-                variant='h5'
-                sx={{py: 1}}
-              >
-                {title}
-              </Typography>
-              <Typography
-                variant='body2'
-              >
-                {description}
-              </Typography>
-            </Card>
-          </Grid>
-        )}
-      </Grid>
+              {section.contents.map(({id, title, description}, index) =>
+                <Grid
+                  item
+                  key={index}
+                  lg={3}
+                  md={4}
+                  sm={6}
+                  xs={12}
+                >
+                  <Card
+                    elevation={3}
+                    sx={{
+                      height: '100%',
+                      p: 2,
+                    }}
+                  >
+                    {id}
+                    <Typography
+                      component='h3'
+                      variant='h5'
+                      sx={{py: 1}}
+                    >
+                      {title}
+                    </Typography>
+                    <Typography
+                      variant='body2'
+                    >
+                      {description}
+                    </Typography>
+                  </Card>
+                </Grid>
+              )}
+            </Grid>
+          </Box>
+        </div>
+      )}
     </Container>
   </SectionContainer>
 );
---
+++ src/components/Skills/tools.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  Code as CodeIcon,
+} from '@mui/icons-material';
+
+const tools = [
+  {
+    id: <CodeIcon />,
+    title: 'React',
+    description: 'Hooks, state management, higher-level components, React Router.',
+  },
+  {
+    id: <CodeIcon />,
+    title: 'Node',
+    description: '',
+  },
+  {
+    id: <CodeIcon />,
+    title: 'npm',
+    description: 'Semantic versioning, publishing to and importing from private registries like Artifactory.',
+  },
+  {
+    id: <CodeIcon />,
+    title: 'Typescript',
+    description: 'Working knowledge (in progress).',
+  },
+  {
+    id: <CodeIcon />,
+    title: 'Webpack',
+    description: '',
+  },
+  {
+    id: <CodeIcon />,
+    title: 'Sass',
+    description: '',
+  },
+];
+
+export {tools};
