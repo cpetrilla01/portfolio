a1b7ce43b3890db2671f026e2dfe4b07bbf300de
Christopher Petrilla <cmpetrilla@gmail.com>
2024-07-16 17:08:27 -0400

    refactor: follow CRA app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './global.css';
+import {theme} from './theme';
+import {ThemeProvider, CssBaseline} from "@mui/material";
+import {
+  Header,
+  Hero,
+  Skills,
+  About,
+  Footer
+} from './components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => (
+  <>
+  <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <Header />
+      <Hero />
+      <Skills />
+      <About />
+      <Footer />
+    </ThemeProvider>
+  </>
   );
-}
 
 export default App;
