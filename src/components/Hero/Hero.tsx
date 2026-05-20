c7372d08dee0e425ee5b52fed6b553f97c1c141e
Christopher Petrilla <cpetrilla01@gmail.com>
2026-05-20 16:25:17 -0400

    update hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,18 +1,37 @@
 import { SectionContainer } from '../common/SectionContainer';
-import { Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
+import heroImage from '../../../assets/development.png';
 
 export const Hero = () => (
   <SectionContainer
     sx={{
-      alignItems: 'center',
       backgroundColor: 'primary.light',
       color: 'white',
-      display: 'flex',
-      justifyContent: 'center',
     }}
   >
-    <Typography sx={{ color: 'inherit' }} variant='subtitle1'>
-      I build enterprise-level Javascript apps
-    </Typography>
+    <Stack
+      spacing={10}
+      direction={{ md: 'row' }}
+      sx={{
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box sx={{ maxWidth: '500px' }}>
+        <Typography sx={{ color: 'inherit' }} variant='subtitle1'>
+          Hi, I'm Chris!
+        </Typography>
+        <Typography sx={{ color: 'inherit' }} variant='body1'>
+          I'm a <strong>front-end software engineer</strong> with over 12 years
+          of experience. I build TypeScript/JavaScript-based web applications
+          using modern development frameworks and tools—primarily React, Vite,
+          React Testing Library, and Vitest. I also have experience with
+          back-end development through NodeJS.
+        </Typography>
+      </Box>
+      <Box>
+        <img src={heroImage} style={{ maxWidth: '100%' }} alt='' />
+      </Box>
+    </Stack>
   </SectionContainer>
 );
--- src/components/common/SectionContainer.tsx
+++ src/components/common/SectionContainer.tsx
@@ -12,15 +12,8 @@ export const SectionContainer = (props: SectionContainerProps) => (
     id={props.id || ''}
     maxWidth={false}
     sx={{
-      minHeight: 'calc(100vh - 56px)',
       py: 8,
       px: 3,
-      '@media only screen and (min-width: 425px)': {
-        minHeight: 'calc(100vh - 48px)',
-      },
-      '@media only screen and (min-width: 600px)': {
-        minHeight: 'calc(100vh - 64px)',
-      },
       ...props.sx,
     }}
   >
