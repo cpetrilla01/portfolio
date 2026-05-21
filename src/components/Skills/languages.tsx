deb95d221f7eee3defda9994488cddcb2d1de54e
Christopher Petrilla <cpetrilla01@gmail.com>
2026-05-21 00:20:50 -0400

    add FontAwesome icons to Languages section

--- src/components/Skills/languages.tsx
+++ src/components/Skills/languages.tsx
@@ -1,30 +1,31 @@
-import {
-  Css as CssIcon,
-  Html as HtmlIcon,
-  Javascript as JavascriptIcon,
-  Javascript as TypescriptIcon,
-} from '@mui/icons-material';
 import type { Skill } from './Skills.tsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCss3Alt,
+  faHtml5,
+  faTypescript,
+  faJs,
+} from '@fortawesome/free-brands-svg-icons';
 
 export const languages: Skill[] = [
   {
-    icon: <TypescriptIcon />,
+    icon: <FontAwesomeIcon icon={faTypescript} size='2xl' />,
     title: 'TypeScript',
     description: 'Primitives, object types, union types, aliases, interfaces.',
   },
   {
-    icon: <JavascriptIcon />,
+    icon: <FontAwesomeIcon icon={faJs} size='2xl' />,
     title: 'JavaScript',
     description:
       'ES6+, event management, several Web APIs (DOM, storage, canvas, Media Capture and Streams, etc.)',
   },
   {
-    icon: <HtmlIcon />,
+    icon: <FontAwesomeIcon icon={faHtml5} size='2xl' />,
     title: 'HTML',
     description: 'HTML5 elements, and semantic, accessible markup.',
   },
   {
-    icon: <CssIcon />,
+    icon: <FontAwesomeIcon icon={faCss3Alt} size='2xl' />,
     title: 'CSS',
     description:
       'CSS3, selectors, specificity, variables, media queries and responsive design, BEM naming conventions.',
