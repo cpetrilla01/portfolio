ea608ec5b2f88dc94989d8ffbfd6187bae0dd970
Christopher Petrilla <cpetrilla01@gmail.com>
2026-06-11 01:39:39 -0400

    add simple unit test for SectionContainer

---
+++ src/components/common/SectionContainer.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import { SectionContainer } from './SectionContainer';
+
+describe('SectionContainer component', () => {
+  it('renders children', () => {
+    const testContent = 'test content';
+    render(<SectionContainer>{testContent}</SectionContainer>);
+    expect(screen.getByText(testContent)).toBeInTheDocument();
+  });
+});
