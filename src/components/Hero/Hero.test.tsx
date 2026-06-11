f440f4bf3d1d7ad61608f4449de8c147c91b7fa4
Christopher Petrilla <cpetrilla01@gmail.com>
2026-06-11 01:43:32 -0400

    simplify describe names to just component name

--- src/components/Hero/Hero.test.tsx
+++ src/components/Hero/Hero.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { Hero } from './Hero.tsx';
 
-describe('Hero component', () => {
+describe('Hero', () => {
   test('renders heading element', () => {
     render(<Hero />);
 
--- src/components/common/SectionContainer.test.tsx
+++ src/components/common/SectionContainer.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { SectionContainer } from './SectionContainer';
 
-describe('SectionContainer component', () => {
+describe('SectionContainer', () => {
   it('renders children', () => {
     const testContent = 'test content';
     render(<SectionContainer>{testContent}</SectionContainer>);
