dc138309df55dfb87a6115d09096d9ca2153f4f7
Christopher Petrilla <cpetrilla01@gmail.com>
2026-06-11 12:21:12 -0400

    add unit tests for Header

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Header } from './Header';
+
+describe('Header', () => {
+  it('renders heading and navigation', () => {
+    render(<Header />);
+
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+    // should be two navigation items (desktop and mobile)
+    expect(screen.getAllByRole('navigation')).toHaveLength(2);
+  });
+
+  it('opens drawer when menu button is clicked', async () => {
+    const user = userEvent.setup();
+
+    render(<Header />);
+
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+
+    await user.click(screen.getByLabelText(/Open drawer/i));
+
+    expect(screen.getByRole('dialog')).toBeVisible();
+
+    await user.click(screen.getByLabelText(/Close drawer/i));
+
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+  });
+
+  it('closes drawer when close button is clicked', async () => {
+    const user = userEvent.setup();
+
+    render(<Header />);
+
+    await user.click(screen.getByLabelText(/Open drawer/i));
+    await user.click(screen.getByLabelText(/Close drawer/i));
+
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+  });
+
+  it('renders a scroll-to-top FAB', () => {
+    render(<Header />);
+
+    expect(screen.getByLabelText(/scroll back to top/i)).toBeInTheDocument();
+  });
+});
